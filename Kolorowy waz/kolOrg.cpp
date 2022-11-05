6b11264c26a9a90273f6784c96b119b4410164f4
Aqven <misiekg999@gmail.com>
2022-11-05 21:26:09 +0100

    alternatywny sposob rozwiazania kol

--- Kolorowy waz/kolOrg.cpp
+++ Kolorowy waz/kolOrg.cpp
@@ -10,16 +10,25 @@ int main(){
     // cin.tie(0);
     int m,p,n;//m -> długość i szerokość planszy p-> liczbę przekąsek na planszy n-> liczbę poleceń do obsłużenia
     cin>>m>>p>>n;
+    vector<string> row;
+    vector<vector<string> > mapa;
 
-    
+    for (int i = 0; i < m; i++)
+    {
+        vector<string> row;
+        for (int j = 0; j < m; j++)
+        {
+            row.push_back(".");
+            //cout<<mapa[i][j]<<" ";
+        }
+        mapa.push_back(row);
+        //cout<<'\n';
+    }
+    mapa[0][0] = "0";//wstawianie weza
     int current_x = 0, current_y = 0;
     vector<string> waz{"0"};
     vector<vector<int> > koordynaty;
     vector<int> pomocnik;
-
-    vector<string> Kolory;
-    vector<vector<int> > koordynaty_Kolorow;
-    vector<int> pomocnik_Kolorow;
     
     pomocnik.push_back(current_y);
     pomocnik.push_back(current_x);
@@ -31,24 +40,23 @@ int main(){
     for (int i = 0; i < p; i++)
     {
         cin>>Wi>>Ki>>Ci;
-
-        pomocnik_Kolorow.push_back(Wi-1);
-        pomocnik_Kolorow.push_back(Ki-1);
-
-        Kolory.push_back(to_string(Ci));
-        koordynaty_Kolorow.push_back(pomocnik_Kolorow);
-        pomocnik_Kolorow.clear();
+        mapa[Wi-1][Ki-1] = to_string(Ci);
     }
-
-        // for (int f = 0; f < koordynaty_Kolorow.size(); f++){
-        //     cout<<"("<<koordynaty_Kolorow[f][0]<<","<<koordynaty_Kolorow[f][1]<<")\n";
-        // }
-        // cout<<"\n";
     
+
+    //  for (int i = 0; i < m; i++)
+    //     {
+    //         for (int j = 0; j < m; j++)
+    //         {
+    //         // mapa[i][j] = '.';
+    //             cout<<mapa[i][j]<<" ";
+    //         }
+    //         cout<<'\n';
+    //     }
 
     char skret;//w którą strone skręca wąż
     int Wj,Kj;//polecenie „Z w′j k′j” oznacza zapytanie o to, jaki kolor fragmentu węża w danym momencie znajduje się w polu w wierszu w′j i kolumnie k′j
-   
+    ofstream out("odp.txt");
 
     for (int i = 0; i < n; i++)
     {
@@ -61,36 +69,30 @@ int main(){
                 {
                     if((koordynaty[j][0] == (Wj-1)) && (koordynaty[j][1] == (Kj-1))){
                         cout<<waz[j]<<endl;
+                        out<<waz[j]<<endl;
                         czyBreak = true;
                         break;
                     }
                 }
                 if(!czyBreak){
                         cout<<-1<<endl;
+                        out<<-1<<endl;
                 }
         }
         else{
 
             if(skret == 'P'){
-                bool czyWykonano = false;
-             for(int g = 0;g < koordynaty_Kolorow.size();g++){
-                if((koordynaty[0][1]+1 == koordynaty_Kolorow[g][1])  && (koordynaty[0][0] == koordynaty_Kolorow[g][0])){
+                if(mapa[current_y][current_x+1] != "."){
                     // waz.insert(waz.begin(),mapa[current_y][current_x+1]);
                     pomocnik.push_back(current_y);
                      pomocnik.push_back(current_x+1);
                     koordynaty.insert(koordynaty.begin(),pomocnik);
                     pomocnik.clear();
-                    waz.insert(waz.begin(),Kolory[g]);
-                    koordynaty_Kolorow.erase(koordynaty_Kolorow.begin()+g);
-                    Kolory.erase(Kolory.begin()+g);
-                    czyWykonano = true;
-                    break;
+                    waz.insert(waz.begin(),mapa[current_y][current_x+1]);
                 }
-             }
-                 if(!czyWykonano){
-                    // koordynaty[0][1]++;
-                   //int tep = ++koordynaty[0][1];
-                       int tep_y = koordynaty[0][0],tep_x = koordynaty[0][1]+1;
+                else{
+                    mapa[current_y][current_x+1] = waz[0];
+                   int tep_y = koordynaty[koordynaty.size()-1][0],tep_x = koordynaty[koordynaty.size()-1][1];
 
                     int kordynaktyKop[koordynaty.size()][2];
 
@@ -102,35 +104,28 @@ int main(){
                     
                     for (int j = waz.size()-1; j >= 1; j--)//---------------------------------
                     {
+                        mapa[koordynaty[j-1][0]][koordynaty[j-1][1]] = waz[j];
+                        //  mapa[koordynaty[j][0]][koordynaty[j][1]] = '.';
                           koordynaty[j][0] = kordynaktyKop[j-1][0];
                          koordynaty[j][1] = kordynaktyKop[j-1][1];
                     }
-                    // koordynaty[0][1] = current_x+1;
-                    koordynaty[0][1] = tep_x;
-                    koordynaty[0][0] = tep_y;
+                    koordynaty[0][1] = current_x+1;
+                     mapa[tep_y][tep_x] = ".";
                 }
                 current_x += 1;
             }
             if(skret == 'L'){
-                bool czyWykonano = false;
-            for(int g = 0;g < koordynaty_Kolorow.size();g++){
-                if((koordynaty[0][1]-1 == koordynaty_Kolorow[g][1]) && (koordynaty[0][0] == koordynaty_Kolorow[g][0])){
-                    // waz.insert(waz.begin(),mapa[current_y][current_x+1]);
+                if(mapa[current_y][current_x-1] != "."){
                     pomocnik.push_back(current_y);
                      pomocnik.push_back(current_x-1);
                     koordynaty.insert(koordynaty.begin(),pomocnik);
                     pomocnik.clear();
-                    waz.insert(waz.begin(),Kolory[g]);
-                    koordynaty_Kolorow.erase(koordynaty_Kolorow.begin()+g);
-                    Kolory.erase(Kolory.begin()+g);
-                    czyWykonano = true;
-                    break;
+                    waz.insert(waz.begin(),mapa[current_y][current_x-1]);
                 }
-            }
-                if(!czyWykonano){
-                //   koordynaty[0][1]--;
-                //    int tep_y = koordynaty[koordynaty.size()-1][0],tep_x = koordynaty[koordynaty.size()-1][1];
-                    int tep_y = koordynaty[0][0],tep_x = koordynaty[0][1]-1;
+                else{
+                  mapa[current_y][current_x-1] = waz[0];
+                   int tep_y = koordynaty[koordynaty.size()-1][0],tep_x = koordynaty[koordynaty.size()-1][1];
+
                     int kordynaktyKop[koordynaty.size()][2];
 
                     for (int k = 0; k < koordynaty.size(); k++)
@@ -138,37 +133,31 @@ int main(){
                             kordynaktyKop[k][0] = koordynaty[k][0];
                              kordynaktyKop[k][1] = koordynaty[k][1];
                     }
-                    
-                    for (int j = waz.size()-1; j >= 1; j--)//---------------------------------
-                    {
-                          koordynaty[j][0] = kordynaktyKop[j-1][0];
-                         koordynaty[j][1] = kordynaktyKop[j-1][1];
-                    }
-                    koordynaty[0][1]= tep_x;
-                    koordynaty[0][0]= tep_y;
+                    // if(waz.size()>1)
+                        for (int j = waz.size()-1; j >= 1; j--)//---------------------------------
+                        {
+                            mapa[koordynaty[j-1][0]][koordynaty[j-1][1]] = waz[j];
+                            //  mapa[koordynaty[j][0]][koordynaty[j][1]] = '.';
+                            koordynaty[j][0] = kordynaktyKop[j-1][0];
+                            koordynaty[j][1] = kordynaktyKop[j-1][1];
+                        }
+                    koordynaty[0][1] = current_x-1;
+                     mapa[tep_y][tep_x] = ".";
                 }
                 current_x -= 1;
             }
             if(skret == 'G'){
-                bool czyWykonano = false;
-                for(int g = 0;g < koordynaty_Kolorow.size();g++) { 
-                 if((koordynaty[0][0]-1 == koordynaty_Kolorow[g][0]) && (koordynaty[0][1] == koordynaty_Kolorow[g][1])){
-                    // waz.insert(waz.begin(),mapa[current_y][current_x+1]);
+                if(mapa[current_y-1][current_x] != "."){
                     pomocnik.push_back(current_y-1);
                      pomocnik.push_back(current_x);
                     koordynaty.insert(koordynaty.begin(),pomocnik);
                     pomocnik.clear();
-                    waz.insert(waz.begin(),Kolory[g]);
-                    koordynaty_Kolorow.erase(koordynaty_Kolorow.begin()+g);
-                    Kolory.erase(Kolory.begin()+g);
-                    czyWykonano = true;
-                    break;
+                    waz.insert(waz.begin(),mapa[current_y-1][current_x]);
                 }
-                }
-                if(!czyWykonano){
-                    // koordynaty[0][0]--;
-                //    int tep_y = koordynaty[koordynaty.size()-1][0],tep_x = koordynaty[koordynaty.size()-1][1];
-                    int tep_y = koordynaty[0][0]-1,tep_x = koordynaty[0][1];
+                else{
+                    mapa[current_y-1][current_x] = waz[0];
+                   int tep_y = koordynaty[koordynaty.size()-1][0],tep_x = koordynaty[koordynaty.size()-1][1];
+
                     int kordynaktyKop[koordynaty.size()][2];
 
                     for (int k = 0; k < koordynaty.size(); k++)
@@ -179,33 +168,28 @@ int main(){
                     
                     for (int j = waz.size()-1; j >= 1; j--)//---------------------------------
                     {
+                        mapa[koordynaty[j-1][0]][koordynaty[j-1][1]] = waz[j];
+                        //  mapa[koordynaty[j][0]][koordynaty[j][1]] = '.';
                           koordynaty[j][0] = kordynaktyKop[j-1][0];
                          koordynaty[j][1] = kordynaktyKop[j-1][1];
                     }
-                    koordynaty[0][0]= tep_y;
-                    koordynaty[0][1] = tep_x;
+                    koordynaty[0][0] = current_y-1;
+                     mapa[tep_y][tep_x] = ".";
                 }
                 current_y -= 1;
             }
             if(skret == 'D'){
-                bool czyWykonano = false;
-                for(int g = 0;g < koordynaty_Kolorow.size();g++)   
-                 if((koordynaty[0][0]+1 == koordynaty_Kolorow[g][0]) && (koordynaty[0][1] == koordynaty_Kolorow[g][1])){
-                    // waz.insert(waz.begin(),mapa[current_y][current_x+1]);
+                if(mapa[current_y+1][current_x] != "."){
                     pomocnik.push_back(current_y+1);
                      pomocnik.push_back(current_x);
                     koordynaty.insert(koordynaty.begin(),pomocnik);
                     pomocnik.clear();
-                    waz.insert(waz.begin(),Kolory[g]);
-                    koordynaty_Kolorow.erase(koordynaty_Kolorow.begin()+g);
-                    Kolory.erase(Kolory.begin()+g);
-                    czyWykonano = true;
-                    break;
+                    waz.insert(waz.begin(),mapa[current_y+1][current_x]);
                 }
-                if(!czyWykonano){
-                //    koordynaty[0][0]++;
-                //    int tep_y = koordynaty[koordynaty.size()-1][0],tep_x = koordynaty[koordynaty.size()-1][1];
-                    int tep_y = koordynaty[0][0]+1,tep_x = koordynaty[0][1];
+                else{
+                   mapa[current_y+1][current_x] = waz[0];
+                   int tep_y = koordynaty[koordynaty.size()-1][0],tep_x = koordynaty[koordynaty.size()-1][1];
+
                     int kordynaktyKop[koordynaty.size()][2];
 
                     for (int k = 0; k < koordynaty.size(); k++)
@@ -216,24 +200,36 @@ int main(){
                     
                     for (int j = waz.size()-1; j >= 1; j--)//---------------------------------
                     {
+                        mapa[koordynaty[j-1][0]][koordynaty[j-1][1]] = waz[j];
+                        //  mapa[koordynaty[j][0]][koordynaty[j][1]] = '.';
                           koordynaty[j][0] = kordynaktyKop[j-1][0];
                          koordynaty[j][1] = kordynaktyKop[j-1][1];
                     }
-                    koordynaty[0][0] = tep_y;
-                    koordynaty[0][1] = tep_x;
+                    koordynaty[0][0] = current_y+1;
+                     mapa[tep_y][tep_x] = ".";
                 }
                 current_y += 1;
             }
-           
-        // for (int f = 0; f < koordynaty.size(); f++){
-        //     cout<<"("<<koordynaty[f][0]<<","<<koordynaty[f][1]<<") "<<waz[f]<<"\n";
-        // }
-        // cout<<'\n';
-        // for (int f = 0; f < koordynaty_Kolorow.size(); f++){
-        //     cout<<"("<<koordynaty_Kolorow[f][0]<<","<<koordynaty_Kolorow[f][1]<<") "<<Kolory[f]<<"\n";
-        // }
-        // cout<<"\n";
-
+           // mapa[current_y][current_x] = '0';
+            
+            // for (int a = 0; a < m; a++)
+            // {
+            //     for (int j = 0; j < m; j++)
+            //     {
+            //     // mapa[i][j] = '.';
+            //         cout<<mapa[a][j]<<" ";
+            //     }
+            // cout<<'\n';
+            // }
+            // cout<<endl;
+            
+            // cout<<endl;
+            // for (int a = 0; a < koordynaty.size(); a++)
+            // {
+            //     cout<<waz[a]<<" ("<<koordynaty[a][0]<<" "<<koordynaty[a][1]<<") "<<endl;
+            // }
+            // cout<<endl;
+            
         }
     }
 
